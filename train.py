348a673e888fbe9ca381b8eea3664ab3b9691042
me <me@example.com>
2020-06-08 22:05:56 +0700

    changed default transfer learning batch size to 8

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ def _main():
             # use custom yolo4_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        batch_size = 32
+        batch_size = 8
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
