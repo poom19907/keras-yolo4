bd265ca95b01c0e86c5b85ef9ccb8290ed40daf8
me <me@example.com>
2020-06-08 22:47:31 +0700

    change paths in test.py main

--- test.py
+++ test.py
@@ -135,9 +135,9 @@ class Yolo4(object):
         return image
 
 if __name__ == '__main__':
-    model_path = 'yolo4_weight.h5'
+    model_path = 'trained_weights_final.h5'
     anchors_path = 'model_data/yolo4_anchors.txt'
-    classes_path = 'model_data/coco_classes.txt'
+    classes_path = 'train_classes.txt'
 
     score = 0.5
     iou = 0.5
